ec39b88cb1214fb40603bfcb850f7245a723449a
Nikhil Raj <nikhil25803@gmail.com>
2024-01-14 19:14:06 +0530

    Footer Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 export default function App() {
   return (
     <>
@@ -9,6 +10,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </Router>
     </>
   );

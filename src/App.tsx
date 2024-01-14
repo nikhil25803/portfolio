4a39c09abdf18cff15eb7eb0acf43da31d26c62b
Nikhil Raj <nikhil25803@gmail.com>
2024-01-14 19:32:33 +0530

    Experience Page Done

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
 export default function App() {
   return (
     <>
@@ -11,6 +12,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
         </Routes>
         <Footer />
       </Router>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,27 +1,17 @@
-import ExperienceCard from "../components/cards/ExperienceCard";
+import ProjectsCard from "../components/cards/ProjectsCard";
 
 const Projects = () => {
   return (
     <section className="bg-backfroundColor w-full font-dmMono">
-      <div className="max-w-[1280px] mx-auto flex flex-col items-start p-5 text-textWhite">
-        <div className="flex flex-row justify-between w-full items-center  py-2">
+      <div className="max-w-[1280px] mx-auto text-textWhite p-5 ">
+        <div className="flex flex-row justify-between items-center">
           <div className="text-2xl md:text-3xl lg:text-4xl ">
-            <span className="text-textPrimary mr-1">/</span>expereince
+            <span className="text-textPrimary mr-1">/</span>projects
           </div>
         </div>
-        <div className="mt-5 space-y-10 mb-5">
-          <ExperienceCard
-            position="Backend Developer Intern"
-            companyName="Edilitics"
-            fromDate="July 2023"
-            toDate="October 2023"
-            points={[
-              "Contributed to building the backend for a no-code data analytic platform, leveraging Python, Fast API for developing REST APIs, MongoDB as a database, and Redis for caching.",
-              "Developed a robust OTP authentication system for secure user authorization and implemented in-app, Slack, and email notifications using Python and FastAPI.",
-              "Transformed data from MongoDB and other databases into structured formats, creating diverse visualizations like heatmaps, box plots, and radar charts to effectively represent complex data.",
-            ]}
-            skillsUsed="Back-End Web Development · REST APIs · MongoDB · FastAPI · Python (Programming Language)"
-          />
+        <div className="mt-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-5 mb-5 ">
+          <ProjectsCard />
+          <ProjectsCard />
         </div>
       </div>
     </section>
